95d02b0fd48d4f74bedfd400b683ca289ec313cc
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-08-28 22:02:46 -0400

    Added get useEffect and recieved info from the api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [char, setChar] = useState([]);
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.
+    get(`https://rickandmortyapi.com/api/character`)
+    .then((res) => {
+      console.log("Res", res.data);
+      setChar(res.data);
+    })
+    .catch((err) => {
+      console.log("Error Occurred", err);
+    })
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {char.map(char => {
+        return <Character name={char.name}/>
+      })}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+
+import React from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+const Character = (props) => {
+  return (
+    <div>
+      <Card>
+        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>{props.name}</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Character;
